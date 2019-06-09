bcacd7b7abf41108d40882321a915c30952de03e
Shane Donnelly <shane@shanedonnelly.com>
2019-06-09 13:54:30 +0100

    Initial commit

--- Knitting Counter/ViewController.swift
+++ Knitting Counter/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rowCount: UITextField!
+    @IBOutlet weak var patternCount: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        rowCount.text = "0"
+        patternCount.text = "0"
     }
 
-
+    @IBAction func rowIncrementTouched(_ sender: Any) {
+        let rowCountString = rowCount.text ?? "0"
+        if let rowCountInt = Int(rowCountString) {
+            rowCount.text = "\(rowCountInt + 1)"
+        }
+    }
+    
+    @IBAction func rowDecrementTouched(_ sender: Any) {
+        let rowCountString = rowCount.text ?? "0"
+        if let rowCountInt = Int(rowCountString) {
+            if rowCountInt > 0 {
+                rowCount.text = "\(rowCountInt - 1)"
+            }
+        }
+    }
+    
+    @IBAction func rowResetTouched(_ sender: Any) {
+        rowCount.text = "0"
+    }
+    
+    @IBAction func patternIncrementTouched(_ sender: Any) {
+        let patternCountString = patternCount.text ?? "0"
+        if let patternCountInt = Int(patternCountString) {
+            patternCount.text = "\(patternCountInt + 1)"
+        }
+    }
+    
+    @IBAction func patternDecrementTouched(_ sender: Any) {
+        let patternCountString = patternCount.text ?? "0"
+        if let patternCountInt = Int(patternCountString) {
+            if patternCountInt > 0 {
+                patternCount.text = "\(patternCountInt - 1)"
+            }
+        }
+    }
+    
+    @IBAction func patternResetTouched(_ sender: Any) {
+        patternCount.text = "0"
+    }
+    
+    
+    
 }
 
