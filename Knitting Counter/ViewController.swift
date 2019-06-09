ccf5a187d7fe0264dd23758e7b6d620dd0255c19
Shane Donnelly <shane@shanedonnelly.com>
2019-06-09 13:04:18 +0100

    Initial Commit

---
+++ Knitting Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Knitting Counter
+//
+//  Created by Shane Donnelly on 09/06/2019.
+//  Copyright © 2019 Shane Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
